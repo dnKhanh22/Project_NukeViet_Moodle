bd64f28b17bafc35a303ffc2f76fe958e1cc8ea6
dnKhanh22 <khanhdoan227@gmail.com>
2024-04-01 00:44:57 +0700

    first commit

---
+++ data/config/admin_config.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * NukeViet Content Management System
+ * @version 4.x
+ * @author VINADES.,JSC <contact@vinades.vn>
+ * @copyright (C) 2009-2024 VINADES.,JSC. All rights reserved
+ * @license GNU/GPL version 2 or any later version
+ * @see https://github.com/nukeviet The NukeViet CMS GitHub project
+ */
+
+if (!defined('NV_MAINFILE')) {
+    exit('Stop!!!');
+}
+
+$array_adminip = [];
+
+$adv_admins = [];
+$adv_admins['21232f297a57a5a743894a0e4a801fc3'] = ['password' => "be05977add575832dc52655d4ad5c42e", 'begintime' => 1711853915, 'endtime' => 0];
